25d3bbaad269af51256df843e9f657f8042caf73
anisha0404 <124439214+anisha0404@users.noreply.github.com>
2025-02-17 11:10:21 +0530

    light_on_off_project

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let bulbon=document.querySelector("#on");
+let bulboff=document.querySelector("#off");
+let imagebulb=document.querySelector("#bulb");
+// console.log(bulbon);
+// console.log(bulboff);
+bulbon.addEventListener("click",()=>{
+    imagebulb.src="bulb-on.png";
+});
+bulboff.addEventListener("click", () => {
+    imagebulb.src = "bulb-off.png";
+  });
+
